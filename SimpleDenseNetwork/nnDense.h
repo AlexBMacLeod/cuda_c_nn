2878c617aae397dfcc32bdf2637b0c75952c3faa
AlexMacLeod <53008875+AlexBMacLeod@users.noreply.github.com>
2021-01-03 13:22:31 -0500

    Update nnDense.h

--- SimpleDenseNetwork/nnDense.h
+++ SimpleDenseNetwork/nnDense.h
@@ -1,3 +1,10 @@
+typedef struct layer {
+    float *weights;
+    float *neurons;
+    int input;
+    int output;
+} layer;
+
 float MSE(float yhat, float y)
 {
     float mse = (y - yhat)**2;
@@ -26,4 +33,15 @@ void printNetwork(layer *hiddenLayers[], char *argv[])
         printf("Layer %d:\nInput %d\tOutput %d\n");
     }
     printf("Output Layer:\nOutput %d", OUTPUT);
+}
+
+void createLayers(layer *hiddenLayers[], char *argv[])
+{
+    for(int i = 0; i < atoi(argv[1]); i++)
+            layer[i].input = {
+                .input = (i==0)COLS:layer[i-1].output, 
+                .output =  (atoi(argv[1]!=i+1)) ? atoi(argv[i+2]) : OUT,
+                .weights = makeWeights(layer[i].input, layer[i].output)}
+        }
+    }
 }
